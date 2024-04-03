6b1357c0fb3fa62505221fe0f7fb13790910c901
George8080 <santisjorge8000@gmail.com>
2024-04-03 02:19:38 -0300

    Test saludo de mañana nº1 (Error)

--- functions.py
+++ functions.py
@@ -1,10 +1,14 @@
+import os
+import datetime
+
+def greetings(name):
+	return ("hola "+name)
+	pass
+
 def palindrome(text):
 	pass
 
 def reverse(txt):
-	pass
-
-def greetings(name):
 	pass
 
 def stop():
---
+++ test.py
@@ -0,0 +1,8 @@
+import pytest
+import os
+import datetime
+from functions import greetings
+
+def test_patched_greetingsMorning(monkeypatch):
+    monkeypatch.setenv('MYDATE', '05-31-2018 05:30:12')
+    assert greetings("Juan") ==  "¡Buenos días Juan!"
