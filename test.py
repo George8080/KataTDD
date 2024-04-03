83a085e2da49d2e088387bf1bbe0b4bd2d40dd7e
George8080 <santisjorge8000@gmail.com>
2024-04-03 03:50:02 -0300

    Test saludo de tarde nº 1 (Test fallido)

--- test.py
+++ test.py
@@ -6,3 +6,7 @@ from functions import greetings
 def test_patched_greetingsMorning(monkeypatch):
     monkeypatch.setenv('MYDATE', '05-31-2018 07:30:12')
     assert greetings("Juan") ==  "¡Buenos días Juan!"
+
+def test_patched_greetingsAfternoon(monkeypatch):
+    monkeypatch.setenv('MYDATE', '08-27-2023 18:32:43')
+    assert greetings("Mario") ==  "¡Buenas tardes Mario!"
